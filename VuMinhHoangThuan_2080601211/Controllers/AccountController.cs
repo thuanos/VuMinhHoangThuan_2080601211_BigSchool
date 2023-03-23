377b460e4cc9ba5e7378298bfcb7348541879fec
Acer2021 <Acer2021@LAPTOP-925ACF1L>
2023-03-24 01:57:03 +0700

    update tuan2

--- VuMinhHoangThuan_2080601211/Controllers/AccountController.cs
+++ VuMinhHoangThuan_2080601211/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace VuMinhHoangThuan_2080601211.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email,Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
---
+++ VuMinhHoangThuan_2080601211/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VuMinhHoangThuan_2080601211.DTOs;
+using VuMinhHoangThuan_2080601211.Models;
+
+namespace VuMinhHoangThuan_2080601211.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+
+            var attendace = new Attendance {
+                CourseId= attendanceDto.CourseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendace);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
