9c44dcc27b8315cd6d2991914871dbfdf9dc495a
Acer2021 <Acer2021@LAPTOP-925ACF1L>
2023-04-03 00:39:34 +0700

    tuan3

---
+++ VuMinhHoangThuan_2080601211/Controllers/FollowingsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using VuMinhHoangThuan_2080601211.DTOs;
+using VuMinhHoangThuan_2080601211.Models;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace VuMinhHoangThuan_2080601211.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+            {
+                return BadRequest("Following Already Exists");
+            }
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(following);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
